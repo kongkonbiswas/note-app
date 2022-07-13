ecc633a525b229174bd7428482f66d50d694b08f
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2022-07-13 18:02:52 +0600

    note app added

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Signup from './src/screens/signup';
+import Signin from './src/screens/signin';
+import Home from './src/screens/home';
+import Edit from './src/screens/edit';
+import Create from './src/screens/create';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false; // not authentacad
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          {
+            user ? (
+            <>
+            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen name='Edit' component={Edit}/>
+            <Stack.Screen name='Create' component={Create}/>
+            </>
+            ) : (
+              <>
+              <Stack.Screen name='Signin' component={Signin} options={{ headerShown: false}}/>
+              <Stack.Screen name='Signup' component={Signup}/>
+              </>
+            )
+          }
+          
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
